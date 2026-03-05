ff8e6302e7585b96ee6bf85ecf9f225c84a763ed
AbhiShake1 <40654066+AbhiShake1@users.noreply.github.com>
2026-03-05 03:32:37 +0000

    chore(cask): bump flo to v0.1.2

--- Casks/flo.rb
+++ Casks/flo.rb
@@ -1,8 +1,8 @@
 cask "flo" do
   # Managed by scripts/update_cask.sh and .github/workflows/release.yml
-  version "0.1.0"
+  version "0.1.2"
   # Placeholder value replaced by the release workflow PR after tagging.
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "0f20fcebbf47f979354c57c034ec1f9714ca22860b9fe326ea6ec927c315958d"
 
   url "https://github.com/AbhiShake1/flo/releases/download/v#{version}/FloApp-#{version}-arm64.dmg"
   name "flo"
